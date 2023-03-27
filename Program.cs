c84cd0e39da21005675def627522c45915189478
hossein rahimi <rahimisafari@gmail.com>
2023-03-27 17:51:41 +0330

    sqlite

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<BugDbContext>(options=>{
     options.UseSqlite("Data Source=bugs.sqlite");
+    
     options.LogTo(Console.WriteLine);
 });
 
